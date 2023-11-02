d1a2a9a744814d9aedb364404d3fd7cbe6e7a921
AyeshaSaif97 <ayesha.saif13@gmail.com>
2023-11-01 21:07:01 -0400

    i have added the planet2

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -2,20 +2,25 @@ package SolarSystem;
 
 public class main {
     public static void main(String[] args) {
-        solarSystemBasics solarsys=new solarSystemBasics();
+        solarSystemBasics solarsys = new solarSystemBasics();
 
-        solarsys.Sun="1";
-        solarsys.Moon="2";
-        solarsys.Planets="2";
-        solarsys.Stars="10";
+        solarsys.Sun = "1";
+        solarsys.Moon = "2";
+        solarsys.Planets = "2";
+        solarsys.Stars = "10";
 
         //***********Feature Sun***********
-        FeatureSun Sun=new FeatureSun();
-        Sun.name="panther";
-        Sun.color="black";
-        Sun.temp="-1223124kelvin";}
+        FeatureSun Sun = new FeatureSun();
+        Sun.name = "panther";
+        Sun.color = "black";
+        Sun.temp = "-1223124kelvin";
 
+        //planet2**
+        Planet2 xaros = new Planet2();
+        xaros.name = "nehaio";
+        xaros.color = "blue";
+        xaros.size = "32432432324324km";
 
 
     }
-
+}
