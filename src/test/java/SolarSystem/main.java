8bed561521b19bbe37e9218bdc18157b5e193c6c
AyeshaSaif97 <ayesha.saif13@gmail.com>
2023-11-01 20:55:49 -0400

    i have added the feature planet

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -2,20 +2,27 @@ package SolarSystem;
 
 public class main {
     public static void main(String[] args) {
-        solarSystemBasics solarsys=new solarSystemBasics();
+        solarSystemBasics solarsys = new solarSystemBasics();
 
-        solarsys.Sun="1";
-        solarsys.Moon="2";
-        solarsys.Planets="2";
-        solarsys.Stars="10";
+        solarsys.Sun = "1";
+        solarsys.Moon = "2";
+        solarsys.Planets = "2";
+        solarsys.Stars = "10";
 
         //***********Feature Sun***********
-        FeatureSun Sun=new FeatureSun();
-        Sun.name="panther";
-        Sun.color="black";
-        Sun.temp="-1223124kelvin";}
+        FeatureSun Sun = new FeatureSun();
+        Sun.name = "panther";
+        Sun.color = "black";
+        Sun.temp = "-1223124kelvin";
 
+        //        PLanet1
+        Planet1 plato = new Planet1();
+        plato.color = "green";
+        plato.name = "abracadabra";
+        plato.temp = "10000000k";
+        plato.radius = "800000km";
 
 
     }
+}
 
